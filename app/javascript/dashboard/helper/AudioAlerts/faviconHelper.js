41681b4b8d4d0acf57da9109deadee7ef03b5a69
Rob Coenen <753704+rcoenen@users.noreply.github.com>
2026-05-21 23:22:40 -0400

    feat(pilot): clear favicon red dot dynamically when conversations are read

--- app/javascript/dashboard/helper/AudioAlerts/faviconHelper.js
+++ app/javascript/dashboard/helper/AudioAlerts/faviconHelper.js
@@ -35,42 +35,42 @@ export const showBadgeOnFavicon = () => {
   });
 };
 
-export const initFaviconSwitcher = () => {
-  const resetFavicon = () => {
-    const badgedFavicons = document.querySelectorAll('.favicon');
-    badgedFavicons.forEach(favicon => {
-      const size =
-        favicon.getAttribute('sizes') || (favicon.sizes && favicon.sizes[0]);
-      if (size) {
-        favicon.href = `/favicon-${size}.png`;
+export const resetFavicon = () => {
+  const badgedFavicons = document.querySelectorAll('.favicon');
+  badgedFavicons.forEach(favicon => {
+    const size =
+      favicon.getAttribute('sizes') || (favicon.sizes && favicon.sizes[0]);
+    if (size) {
+      favicon.href = `/favicon-${size}.png`;
 
-        // Force redrawing the original favicon by re-appending it
-        const parent = favicon.parentNode;
-        if (parent) {
-          const next = favicon.nextSibling;
-          parent.removeChild(favicon);
-          parent.insertBefore(favicon, next);
-        }
-      }
-    });
-
-    // Restore all removed high-resolution or custom branded icons
-    while (removedFavicons.length > 0) {
-      const item = removedFavicons.shift();
-      if (item.parent && !item.element.parentNode) {
-        item.parent.insertBefore(item.element, item.next);
-
-        // Force a redraw of the restored favicon
-        const parent = item.element.parentNode;
-        if (parent) {
-          const next = item.element.nextSibling;
-          parent.removeChild(item.element);
-          parent.insertBefore(item.element, next);
-        }
+      // Force redrawing the original favicon by re-appending it
+      const parent = favicon.parentNode;
+      if (parent) {
+        const next = favicon.nextSibling;
+        parent.removeChild(favicon);
+        parent.insertBefore(favicon, next);
       }
     }
-  };
+  });
 
+  // Restore all removed high-resolution or custom branded icons
+  while (removedFavicons.length > 0) {
+    const item = removedFavicons.shift();
+    if (item.parent && !item.element.parentNode) {
+      item.parent.insertBefore(item.element, item.next);
+
+      // Force a redraw of the restored favicon
+      const parent = item.element.parentNode;
+      if (parent) {
+        const next = item.element.nextSibling;
+        parent.removeChild(item.element);
+        parent.insertBefore(item.element, next);
+      }
+    }
+  }
+};
+
+export const initFaviconSwitcher = () => {
   document.addEventListener('visibilitychange', () => {
     if (document.visibilityState === 'visible') {
       resetFavicon();
